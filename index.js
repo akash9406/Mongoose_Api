53087baa8cccd812c0163a54550c0283bcd862a2
Akash Malakar <AkashMalakar3.3@gmail.com>
2024-02-02 21:10:13 +0530

    ejs

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const server = express();
 const productRouter = require("./routes/product");
 // const userRouter = require("./routes/user");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 //schema
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database connected");
   } catch (error) {
     console.log(error);
@@ -18,13 +18,14 @@ const connect = async () => {
 };
 
 connect();
-
+server.use(cors());
 server.use(express.json());
 server.use(morgan("default"));
 server.use(express.static("public"));
 server.use("/products", productRouter.router);
 // server.use("/users", userRouter.router);
-
+// server.use('*',(req,res) => { res.sendFile(path.resolve(__dirname,"build/index.html"))}) use for set
+// to directs to route to react route
 server.listen(process.env.PORT, () => {
   console.log("server started");
 });
--- routes/product.js
+++ routes/product.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router
   .post("/", productController.createProduct)
   .get("/", productController.getAllProducts)
+  .get("/ssr", productController.getAllProductsSSR)
+  .get("/add", productController.getAddForm)
   .get("/:id", productController.getProduct)
   .put("/:id", productController.replaceProduct)
   .patch("/:id", productController.updateProduct)
